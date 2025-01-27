97340dc4b6bacdc285755feef97b756309a23b01
billn <billnyetherussianspy679@gmail.com>
2025-01-27 13:38:55 +0300

    saving changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
-function App() {
-  return (
-    <>
-      <header>
-        
-      </header>
-    </>
-  );
-};
+export default function App(){
+    const [users, setUsers] = useState([]);
 
-export default App;
+    useEffect(() =>{
+        fetch("http://localhost:4000/users")
+            .then(r => r.json())
+            .then(data => setUsers(data))
+            .catch(error => console.error(error));
+    }, []);
+
+    return(
+        <>
+            <header>
+                <NavBar />
+            </header>
+            <Outlet context={users}/>
+        </>
+    );
+}
